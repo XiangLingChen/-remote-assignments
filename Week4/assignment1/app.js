6fce87263834b83f8d59200d235646bf324e5629
XiangLingChen <belle19950901@gmail.com>
2019-07-08 00:14:23 +0800

    remote assignment
    week4

---
+++ Week4/assignment1/app.js
@@ -0,0 +1,8 @@
+function delayedResult(n1, n2, delayTime, callback){ 
+    const result = n1 + n2;
+   
+    setTimeout(callback(result), delayTime);    
+}; 
+
+delayedResult( 4, 5, 3000, function(result){ console.log(result); } ); 
+delayedResult(-5, 10, 2000, function(result){ window.alert(result); });
